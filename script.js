147adb9c9d3232dc457461c171159d32fe28584a
Jekoff <jlanloze@gmail.com>
2023-10-16 02:14:32 +0200

    commit

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const notesContainer = document.querySelector(".notes-container");
+const createBtn = document.querySelector(".btn");
+let notes = document.querySelectorAll("input-box");
+
+createBtn.addEventListener("click", () =>{
+    let inputBox = document.createElement("p");
+    let img = document.createElement("img");
+    inputBox.setAttribute("contenteditable", "true");
+    notesContainer.appendChild(inputBox).appendChild(img);
+})
